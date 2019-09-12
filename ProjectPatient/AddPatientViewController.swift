c86b532bfb29145e400d3db5594c69d8669c58eb
Michael <sawlani.michael@yahoo.com>
2019-09-12 13:48:39 -0400

    add patients
    added field checks when adding patients to prevent blank appointments and all faluts by user

--- ProjectPatient/AddPatientViewController.swift
+++ ProjectPatient/AddPatientViewController.swift
@@ -14,15 +14,25 @@ class AddPatientViewController: UIViewController {
     @IBOutlet weak var ageTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        navigationItem.hidesBackButton = true;
+        
+        let backNavButton = UIBarButtonItem(title: "<", style: .done, target: self, action: #selector(BackButton))
+        
         let doneNavButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(DoneButton))
         
         self.navigationItem.rightBarButtonItem = doneNavButton
+        self.navigationItem.leftBarButtonItem = backNavButton
         
         // Do any additional setup after loading the view.
     }
     
     @objc func DoneButton(){
+        
+        if CheckInput() == false{
+            return
+        }
+        
         let patient = createPatient()
         guard let home = self.navigationController?.viewControllers.first as? PatientListViewController else {
             return
@@ -32,10 +42,55 @@ class AddPatientViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
     
+    @objc func BackButton(){
+        let alert = UIAlertController(title: "Going Back?", message: "Doing this will lose all data on this page", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "NO", style: .default, handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "YES", style: .default, handler: {(action) in
+            self.navigationController?.popViewController(animated: true)
+            
+        }))
+        
+        self.present(alert, animated: true)
+        
+    }
+    
     
     func createPatient() -> Patient {
-        var patient = Patient(name: nameTextField.text!, age: ageTextField.text!)
+        let patient = Patient(name: nameTextField.text!, age: ageTextField.text!)
         return patient
+    }
+    
+    func CheckInput() -> Bool{
+        let check = true
+        if self.nameTextField.text!.isEmpty && !self.ageTextField.text!.isEmpty{
+            let alert = UIAlertController(title: "Failed to add patient", message: "Name is empty, must enter a name", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+
+            return false
+        }
+        
+        if self.ageTextField.text!.isEmpty && !self.nameTextField.text!.isEmpty{
+            let alert = UIAlertController(title: "Failed to add patient", message: "Age is empty, must enter a age", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+
+            return false
+        }
+        
+        if self.ageTextField.text!.isEmpty && self.nameTextField.text!.isEmpty{
+            let alert = UIAlertController(title: "Failed to add patient", message: "Please fill out the information", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            
+            return false
+        }
+        
+        return check
     }
 
 }
