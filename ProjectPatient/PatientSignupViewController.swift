23b96e8035e8ab84ff816a065dba1cf60a7a542b
Michael <sawlani.michael@yahoo.com>
2019-10-16 13:56:12 -0400

    patient signup
    started making a patient sign up VC

---
+++ ProjectPatient/PatientSignupViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PatientSignupViewController.swift
+//  ProjectPatient
+//
+//  Created by Michael Sawlani on 10/16/19.
+//  Copyright © 2019 Michael Sawlani. All rights reserved.
+//
+
+import UIKit
+
+class PatientSignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
